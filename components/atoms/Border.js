05b61ca176be390dd3bdb3f66fabb716dc20f65e
I. Bats <bats@ibats.co>
2022-03-20 21:51:19 +0100

    refactor Border

--- components/atoms/Border.js
+++ components/atoms/Border.js
@@ -1,5 +1,12 @@
-export default function Border() {
-    return(
-        <div className="border-[#182f45] border-b border-dotted"></div>
-    )
+import { twClass } from "utils/twClass";
+
+export default function Border({ className }) {
+  return (
+    <div
+      className={twClass(
+        "border-[#182f45] border-b border-dotted",
+        className && `${className}`
+      )}
+    ></div>
+  );
 }
