8ec3a06e2c410740a062b35f5accb231ea4aba83
I. Bats <bats@ibats.co>
2022-03-20 23:45:37 +0100

    refactor headerbutton better copy

--- components/atoms/HeaderButton.js
+++ components/atoms/HeaderButton.js
@@ -1,6 +1,6 @@
 export default function HeaderButton() {
   return (
-    <button className="p-2 text-sm font-semibold text-white uppercase bg-gradient-to-r from-[#e052a0] to-[#f15c41] hover:bg-gradient-to-r hover:from-[#3ec7e0] hover:to-[#526bf4] rounded-sm">
+    <button className="py-2 px-2.5 text-sm font-semibold text-white uppercase bg-gradient-to-r from-[#e052a0] to-[#f15c41] hover:bg-gradient-to-r hover:from-[#3ec7e0] hover:to-[#526bf4] rounded-sm">
       New post
     </button>
   );
