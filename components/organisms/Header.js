708199d1a25ce2d4848daf810e249829638f39c6
I. Bats <bats@ibats.co>
2022-03-20 21:51:32 +0100

    refactor Header

--- components/organisms/Header.js
+++ components/organisms/Header.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import FilterNav from '@/molecules/FilterNav';
+import HeaderButton from '@/atoms/HeaderButton';
 
 const headerItems = [
   { name: "Popular", href: "/" },
@@ -13,8 +14,9 @@ export default function Header() {
   const router = useRouter();
 
   return (
-    <header className="p-4">
+    <header className='flex items-center justify-between'>
     <FilterNav props={headerItems} router={router}></FilterNav>
+    <HeaderButton />
     </header>
   );
 }
