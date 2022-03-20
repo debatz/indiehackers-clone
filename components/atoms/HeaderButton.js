c5fc35c1ba8c99582cb3d8fea94c0e90c0182342
I. Bats <bats@ibats.co>
2022-03-20 15:32:50 +0100

    add HeaderButton

---
+++ components/atoms/HeaderButton.js
@@ -0,0 +1,7 @@
+export default function HeaderButton() {
+  return (
+    <button className="p-2 text-sm font-semibold text-white uppercase bg-gradient-to-r from-[#e052a0] to-[#f15c41] rounded-sm">
+      New post
+    </button>
+  );
+}
