05d4291bd6d46b9e45b2985cb59154ca3405f0e3
I. Bats <bats@ibats.co>
2022-03-21 22:12:44 +0100

    add paginationtitle atom

---
+++ components/atoms/PostLoop/PaginationTitle.js
@@ -0,0 +1,12 @@
+import { HeroIcon } from "../HeroIcon";
+
+export default function PaginationTitle({ children }) {
+  return (
+    <div className='flex items-center space-x-1'>
+      <span className="text-sm font-semibold uppercase text-[#b6cce2] opacity-50">
+        {children}
+      </span>
+      <HeroIcon className="w-3.5 text-[#b6cce2] opacity-50" icon="ArrowRightIcon" />
+    </div>
+  );
+}
