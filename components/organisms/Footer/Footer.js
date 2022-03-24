f67cd38da49deb1973f39bedb581f1143ed289a2
I. Bats <bats@ibats.co>
2022-03-24 22:45:39 +0100

    refactor footer

--- components/organisms/Footer/Footer.js
+++ components/organisms/Footer/Footer.js
@@ -1,14 +1,33 @@
 import FooterColumn from "@/components/molecules/Footer/FooterColumn";
 import FooterCTA from "@/components/molecules/Footer/FooterCTA";
+import FooterInfo from "@/components/atoms/Footer/FooterInfo";
+import FooterAttribution from "@/components/molecules/Footer/FooterAttribution";
+import FooterLegal from "@/components/molecules/Footer/FooterLegal";
 
-export default function Footer({props, router}) {
+export default function Footer({ props, router }) {
   return (
     <div>
       <FooterCTA />
-      <div className="grid grid-cols-2 gap-12">
+      <div className="grid grid-cols-3 gap-4 py-12">
         {props.map((item, index) => {
-          return <FooterColumn key={index} title={item.title} props={item.links} router={router} />;
+          return (
+            <FooterColumn
+              key={index}
+              title={item.title}
+              props={item.links}
+              router={router}
+            />
+          );
         })}
+      </div>
+      <div className="space-y-3">
+        <FooterAttribution />
+        <FooterInfo>
+          Learn from the founders behind hundreds of profitable online
+          businesses, and connect with others who are starting and growing their
+          own companies.
+        </FooterInfo>
+        <FooterLegal />
       </div>
     </div>
   );
