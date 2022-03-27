a9008ef139329e8af0c8421b7c4947a28c82d42c
I. Bats <bats@ibats.co>
2022-03-27 22:05:37 +0200

    refactor slug

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -2,11 +2,21 @@ import { supabase } from "utils/supabase";
 import urlSlug from "url-slug";
 
 export default function Post({ post }) {
+  console.log(post);
   return (
     <div>
       <h1 className="mt-4 text-5xl font-semibold tracking-wide">
         {post.title}
       </h1>
+      <ul>
+      {post.comments.map((item, index) => {
+        return(
+          <li key={index}>
+            <p>{item.content}</p>
+          </li>
+        )
+      })}
+      </ul>
     </div>
   );
 }
@@ -21,15 +31,19 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const { slug } = params;
-  const { data } = await supabase
+  const { data: post, error } = await supabase
     .from("posts")
-    .select()
-    .filter("slug", "eq", slug)
+    .select("*, comments(*)")
+    .eq("slug", params.slug)
     .single();
+
+  if (error) {
+    console.log(error.message);
+  }
+
   return {
     props: {
-      post: data,
+      post,
     },
   };
 }
