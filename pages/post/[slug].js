21dc7a194be0c54e6ffb79b74cd04dba6d77cf12
I. Bats <bats@ibats.co>
2022-03-28 21:00:29 +0200

    refactor slug

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,21 +1,60 @@
 import { supabase } from "utils/supabase";
-import urlSlug from "url-slug";
+import SlugPostContent from "@/components/atoms/Slug/SlugPostContent";
+import SlugPostHeader from "@/components/molecules/Slug/SlugPostHeader";
+import { dateFormatter } from "utils/dateFormatter";
+import SlugComments from "@/components/organisms/Slug/SlugComments";
+import ReactMarkdown from "react-markdown";
+import CommentsAuthorBadge from "@/components/atoms/Slug/Comments/CommentsAuthorBadge";
+import CommentsUpvotes from "@/components/atoms/Slug/Comments/CommentsUpvotes";
 
 export default function Post({ post }) {
-  console.log(post);
+  const diffDays = (date, otherDate) =>
+    Math.ceil(Math.abs(date - otherDate) / (1000 * 60 * 60 * 24));
+  const currentDate = new Date();
   return (
-    <div>
-      <h1 className="mt-4 text-5xl font-semibold tracking-wide">
-        {post.title}
-      </h1>
-      <ul>
-      {post.comments.map((item, index) => {
-        return(
-          <li key={index}>
-            <p>{item.content}</p>
-          </li>
-        )
-      })}
+    <div className="mx-4 space-y-12">
+      <SlugPostHeader title={post.title} author={post.author} />
+      <SlugPostContent>{post.content}</SlugPostContent>
+      <div className="flex flex-wrap items-center space-x-1 text-[#d1d5db] text-xl">
+        <span>—</span>
+        <span className="text-[#5aa3ed]">{post.author}</span>
+        <span>posted to</span>
+        <div className="flex items-center space-x-1">
+          <img
+            className="w-6 h-6"
+            src={post.collection_badge}
+            alt={post.collection_name}
+          />
+          <span className="text-[#5aa3ed]">{post.collection_name}</span>
+        </div>
+        <span>on</span>
+        <span>{dateFormatter(post.created_at)}</span>
+      </div>
+      <SlugComments author={post.author} />
+
+      <ul className="space-y-6">
+        {post.comments.map((item, index) => {
+          return (
+            <li key={index}>
+              <div className="flex space-x-2">
+                <CommentsUpvotes props={item.upvotes} />
+                <ReactMarkdown className="prose prose-lg prose-invert">
+                  {item.content}
+                </ReactMarkdown>
+              </div>
+              <div className="flex items-center space-x-2">
+                <CommentsAuthorBadge authorBadge={item.author_badge} />
+                <span>{item.author}</span>
+                <span>·</span>
+                <span>
+                  {diffDays(currentDate, new Date(item.created_at))} days ago
+                </span>
+                <span>·</span>
+                <span className="font-semibold text-white">REPLY</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
