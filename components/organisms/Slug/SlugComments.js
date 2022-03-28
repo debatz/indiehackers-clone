cd42d3625b01b93528ba70e5605cac2a326aa509
I. Bats <bats@ibats.co>
2022-03-28 21:00:21 +0200

    add slugcomments

---
+++ components/organisms/Slug/SlugComments.js
@@ -0,0 +1,9 @@
+import SlugCommentsBox from '@/components/molecules/Slug/SlugCommentsBox';
+
+export default function SlugComments({author}) {
+    return(
+        <div>
+            <SlugCommentsBox author={author} />
+        </div>
+    )
+}
